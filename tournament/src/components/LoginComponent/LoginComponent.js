fd3d6701204cd1bdbca8f085148e44ff37144dee
Alan Altindis <altindisa@gmail.com>
2023-11-07 16:05:49 +0600

    redirec+t

--- tournament/src/components/LoginComponent/LoginComponent.js
+++ tournament/src/components/LoginComponent/LoginComponent.js
@@ -2,31 +2,42 @@ import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { useDispatch } from "react-redux";
 import { connect } from "react-redux";
-import {loginUser} from "../../redux/actions/actions";
+import { loginUser } from "../../redux/actions/actions";
+import { redirect } from "react-router-dom";
 
-const LoginComponent = ({loginUser}) => {
+const LoginComponent = ({ loginUser }) => {
+  const handleLogin = (username, password) => {
+    let loginData = {
+      username: document.getElementById("#loginUsername").value,
+      password: document.getElementById("#loginPassword").value,
+    };
+
+    loginUser(username, password);
+
+     redirect("/profile");
+  };
   return (
     <Container>
       <Form>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Имя пользователя</Form.Label>
-          <Form.Control type="text" placeholder="Введите имя пользователя" id="loginUsername"/>
+          <Form.Control
+            type="text"
+            placeholder="Введите имя пользователя"
+            id="loginUsername"
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Пароль</Form.Label>
-          <Form.Control type="password" placeholder="Введите пароль" id="loginPassword"/>
+          <Form.Control
+            type="password"
+            placeholder="Введите пароль"
+            id="loginPassword"
+          />
         </Form.Group>
-        <Button variant="primary" type="submit" onClick={() => {
-            let loginData = {
-                username: document.getElementById('#loginUsername').value,
-                password: document.getElementById('#loginPassword').value
-            }
-
-            loginUser(loginData.username, loginData.password);
-        }}>
-            Войти
+        <Button variant="primary" type="submit" onClick={handleLogin}>
+          Войти
         </Button>
       </Form>
     </Container>
@@ -34,10 +45,9 @@ const LoginComponent = ({loginUser}) => {
 };
 
 const mapStateToProps = (state) => {
-    return {};
-  };
-  
-  const mapDispatchToProps = { loginUser };
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(LoginComponent);
-  
+  return {};
+};
+
+const mapDispatchToProps = { loginUser };
+
+export default connect(mapStateToProps, mapDispatchToProps)(LoginComponent);
