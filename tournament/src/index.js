d554e70a27c413bfd61013a120d5f9dc9c9956ae
Alan Altindis <altindisa@gmail.com>
2024-01-08 01:50:37 +0600

    rtk changes

--- tournament/src/index.js
+++ tournament/src/index.js
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
-<<<<<<< Updated upstream
-import store from './redux/store/store.js';
-=======
 import store from './redux/store/store.ts';
->>>>>>> Stashed changes
 import './i18n.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
